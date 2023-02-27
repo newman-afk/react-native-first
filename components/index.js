a12b4e542d44a1e1fd30f8b9afcfa317169cbd99
newman-afk <84593706+newman-afk@users.noreply.github.com>
2023-02-27 12:58:57 +0800

    first commit

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+  FocusedStatusBar,
+  HomeHeader,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+};
